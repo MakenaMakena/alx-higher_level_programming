8bb00bed39c34b4842ded5ea9cd3af525e6c2624
MakenaMakena <wambuigetrude21@gmail.com>
2022-08-29 11:27:49 -0700

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    else idx > len(my_list)-1:
+    if idx > len(my_list)-1:
         return my_list
+    else:
+        return (my_list, idx, element)
